a461e2d4b5c681b5ceeb6b3ef6f4ae581953c779
VitaliiBaryliuk <vitaliyua912@gmail.com>
2018-12-04 21:29:15 +0200

    Files uploaded

---
+++ src/js/script.js
@@ -0,0 +1,98 @@
+(function ticTacToe() {
+  const container = document.querySelector('.container');
+  const fields = {};
+  let lastPlayer = '0';
+
+  const conditions = {
+    0: [1, 2, 3],
+    1: [3, 4, 5],
+    2: [6, 7, 8],
+    3: [0, 3, 6],
+    4: [1, 4, 7],
+    5: [2, 5, 8],
+    6: [0, 4, 8],
+    7: [2, 4, 6],
+  };
+
+  const createElement = (tag, parentElem, elemClass) => {
+    const createdElem = document.createElement(tag);
+    parentElem.appendChild(createdElem);
+    createdElem.classList.add(elemClass);
+    return createdElem;
+  };
+
+  const tikTakToe = createElement('div', container, 'tic-tac-toe');
+
+  const createFields = () => {
+    for (let i = 0; i < 9; i += 1) {
+      fields[i] = {
+        value : '',
+      };
+    }
+  };
+
+  const setBorders = (objCell, randeredCell) => {
+    if (objCell < 6) {
+      randeredCell.classList.add('border-bottom');
+    }
+    if (objCell === '0' || objCell === '1' || objCell === '3' || objCell === '4' || objCell === '6' || objCell === '7') {
+      randeredCell.classList.add('border-right');
+    }
+  };
+
+  const randerFields = (field) => {
+    tikTakToe.innerHTML = '';
+    for (cell in field) {
+      const cellItem = createElement('div', tikTakToe, 'tic-tac-toe__cell');
+      cellItem.setAttribute('data-cell', `${cell}`);
+      cellItem.innerHTML = field[cell].value;
+      setBorders(cell, cellItem);
+    }
+    const againButton = createElement('div', tikTakToe, 'tic-tac-toe__again-button');
+    againButton.innerHTML = 'Try again';
+    againButton.dataset.button = 'again';
+  };
+
+  const setCurrentSign = (event) => {
+    if (lastPlayer === '0') {
+      fields[event.target.dataset.cell].value = 'x';
+      lastPlayer = 'x';
+    } else {
+      fields[event.target.dataset.cell].value = '0';
+      lastPlayer = '0';
+    }
+    Object.freeze(fields[event.target.dataset.cell]);
+  };
+
+  const checkWin = (last) => {
+    for (conditon in conditions) {
+      const cond = conditions[conditon];
+      let counter = 0;
+      for (coord of cond) {
+        if (fields[coord].value === last) {
+          counter++;
+          if(counter === 3) {
+            // alert(`win ${last}`);
+            setTimeout(function() { alert(`win ${last}`) }, 200);
+            lastPlayer = '0';
+          }
+        }
+      }
+      counter = 0;
+    }
+  };
+
+  const ticTacToeAddEventListener = (event) => {
+    if (event.target.dataset.button === 'again') {
+      createFields();
+      randerFields(fields);
+    }
+    setCurrentSign(event);
+    randerFields(fields);
+    checkWin(lastPlayer);
+    console.log(fields);
+  };
+  createFields();
+  randerFields(fields);
+  tikTakToe.addEventListener('click', ticTacToeAddEventListener);
+}());
